12f248a892dfeb95ef480bb8536673b9659eff80
moi15moi <moi15moismokerlolilol@gmail.com>
2026-09-07 19:12:40 -0400

    [fontconfig] Set ac_cv_va_copy to fix compilation

--- cmake/fontconfig.cmake
+++ cmake/fontconfig.cmake
@@ -5,6 +5,13 @@ ExternalProject_Add(ep_fontconfig
     INSTALL_DIR ${CMAKE_BINARY_DIR}
     CONFIGURE_COMMAND
         ${CMAKE_COMMAND} -E env ${PLATFORM_CONFIGURE_ENV}
+        # m4/va_copy.m4 AC_RUN_IFELSE has no cross-compiling fallback.
+        # Due to that, the check fails with: 
+        #   checking for va_copy() function... configure: error: in `PATH/libass-cmake/build/ep_fontconfig-prefix/src/ep_fontconfig-build':
+        #   configure: error: cannot run test program while cross compiling
+        # To avoid this failure, set its cache variable to skip the runtime check.
+        # The NDK toolchain always provides C99 va_copy().
+        ac_cv_va_copy=C99
         <SOURCE_DIR>/<SOURCE_SUBDIR>/configure ${PLATFORM_BUILD_AND_HOST}
         --prefix=<INSTALL_DIR>
         --enable-static
